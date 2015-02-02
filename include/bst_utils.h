1f6a035af37c00375db7a4010bb0ca165c6338ea
Nikola Hardi <atlantic777@lugons.org>
2015-02-02 02:33:06 +0100

    Extracted the bst_tuils.h

---
+++ include/bst_utils.h
@@ -0,0 +1,44 @@
+#ifndef H_BST_UTILS_
+#define H_BST_UTILS_
+
+#include "vgp.h"
+#include <stdio.h>
+
+#define F   5
+#define LEN 200
+
+typedef struct {
+    vgp_parkiranje entry;
+} overflow_block;
+
+typedef struct {
+    vgp_parkiranje entries[F];
+    int n_overflows;
+    overflow_block *first_overflow;
+} main_block;
+
+typedef struct {
+    int  block_addr;
+    char  key[LEN_E_BR+1];
+} index_entry;
+
+typedef struct {
+    index_entry entries[2];
+    int less;
+    int more;
+} stored_index_block;
+
+typedef struct {
+    index_entry entries[2];
+    struct index_block *less;
+    struct index_block *more;
+} index_block;
+
+int   search_bst(char *key, FILE *f);
+void store_bst(index_block *node, FILE *f, int level, int *offset);
+
+// The hell is this?
+stored_index_block tmp;
+
+
+#endif
--- src/init_db.c
+++ src/init_db.c
@@ -6,6 +6,7 @@
 #include <string.h>
 #include <stdlib.h>
 #include <math.h>
+#include "bst_utils.h"
 
 // TODO brisanje elemenata
 // TODO status polje u vgp_parkiranje
@@ -13,172 +14,14 @@
 // TODO balanced BST reda 2 - done
 // TODO pametnija reorganizacija baze
 // TODO proveriti tip povezivanja prekoračilaca (indirektno)
+//
+FILE *f        ;
+FILE *seq;
+FILE *acct;
+FILE *idx;
 
-#define F 5
-#define LEN 200
-
-typedef struct {
-    vgp_parkiranje entry;
-} overflow_block;
-
-typedef struct {
-    vgp_parkiranje entries[F];
-    int n_overflows;
-    overflow_block *first_overflow;
-} main_block;
-
-typedef struct {
-    int  block_addr;
-    char  key[LEN_E_BR+1];
-} index_entry;
-
-typedef struct {
-    index_entry entries[2];
-    int less;
-    int more;
-} stored_index_block;
-
-typedef struct {
-    index_entry entries[2];
-    struct index_block *less;
-    struct index_block *more;
-} index_block;
-
-stored_index_block tmp;
-
-void store_bst(index_block *node, FILE *f, int level, int *offset)
+void csv_to_serial()
 {
-    int left  = node->less ? 2*(*offset)+1 : -1;
-    int right = node->more ? 2*(*offset)+2 : -1;
-
-    printf("stored on %d, left is %d right is %d\n", *offset, left, right);
-    (*offset)++;
-
-    memcpy(tmp.entries, node->entries, sizeof(index_entry)*2);
-    tmp.less = left;
-    tmp.more = right;
-
-    printf("keys are: %s %s\n", tmp.entries[0].key, tmp.entries[1].key);
-    printf("block addrs: %d %d\n", tmp.entries[0].block_addr, tmp.entries[1].block_addr);
-
-    fwrite(&tmp, sizeof(stored_index_block), 1, f);
-
-    if(node->less)
-    {
-        printf("less %p\n", node->less);
-        store_bst((index_block*)node->less, f, level+1, offset);
-        left = 2*(*offset)+1;
-    }
-
-    if(node->more)
-    {
-        printf("more %p\n", node->more);
-        store_bst((index_block*)node->more, f, level+1, offset);
-        right = 2*(*offset)+2;
-    }
-
-}
-
-int search_bst(char *key, FILE *f)
-{
-    int offset = -1;
-    stored_index_block tmp, l, m;
-    int i;
-    int k = atoi(key);
-    int e1, e2;
-
-    rewind(f);
-
-    fread(&tmp, sizeof(index_block), 1, f);
-
-    do {
-        printf("block addrs: %d %d\n", tmp.entries[0].block_addr, tmp.entries[1].block_addr);
-        e1 = (tmp.entries[0].block_addr != -1) ? atoi( tmp.entries[0].key ) : -1;
-        e2 = (tmp.entries[1].block_addr != -1) ? atoi( tmp.entries[1].key ) : -1;
-
-        printf("%d %d\n", e1, e2);
-
-        if( (k < e1) && (tmp.less != -1) )
-        {
-            puts("go less");
-            fseek(f, tmp.less*sizeof(stored_index_block), SEEK_SET );
-            fread(&tmp, sizeof(stored_index_block), 1, f);
-        }
-        else if( (k >= e1) && (k < e2) || (e2 == -1) )
-        {
-            puts("first area");
-            return tmp.entries[0].block_addr;
-        }
-        else if( tmp.more != -1 )
-        {
-            fseek(f, tmp.more*sizeof(stored_index_block), SEEK_SET );
-            fread(&m, sizeof(stored_index_block), 1, f);
-
-            if( k < atoi(m.entries[0].key) )
-            {
-                puts("second area");
-                return tmp.entries[1].block_addr;
-            }
-            else
-            {
-                puts("go more");
-                tmp = m;
-            }
-
-        }
-    } while( tmp.less || tmp.more );
-
-    return offset;
-
-}
-
-void create_bst(index_block *head, main_block *main_blocks_arr, int start, int end, int level)
-{
-    if(start > end) return;
-
-    if( head == NULL ) return;
-
-    int mid = start + (end - start)/2;
-
-    printf("l %d ", level);
-    printf("storing info about blocks %d ", mid);
-
-
-    head->entries[0].block_addr = mid;
-    strcpy(head->entries[0].key, main_blocks_arr[mid].entries[0].e_br);
-
-    if(mid+1 <= end)
-    {
-        printf("%d\n", mid+1);
-        head->entries[1].block_addr =  mid+1;
-        strcpy(head->entries[1].key, main_blocks_arr[mid+1].entries[0].e_br);
-    }
-    else
-    {
-        head->entries[1].block_addr = -1;
-        head->entries[1].key[0]     = 0;
-        printf("NULL\n");
-    }
-
-    if( start < mid )
-    {
-        head->less = malloc( sizeof(index_block) );
-        create_bst( (index_block*)head->less, main_blocks_arr, start, mid-1, level+1 );
-    }
-    else
-    {
-        head->less = NULL;
-    }
-
-    if( (mid+1) < end )
-    {
-        head->more = malloc( sizeof(index_block) );
-        create_bst( (index_block*)head->more, main_blocks_arr, mid+2, end  , level+1);
-    }
-    else
-    {
-        head->less = NULL;
-    }
 }
 
 int create_from_data()
